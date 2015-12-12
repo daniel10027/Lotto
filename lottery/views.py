19f06d69077c9b08dfb73927b5dd297647116b20
Javier Rojas <jerojasro@devnull.li>
2015-12-12 21:22:47 +0100

    use new manager to obtain list of available lotteries

--- lottery/views.py
+++ lottery/views.py
@@ -1,5 +1,4 @@
 from django.views.generic import ListView
-from django.utils.timezone import now
 
 from lottery.models import Lottery
 
@@ -8,8 +7,4 @@ class AvailableLotteries(ListView):
     context_object_name = "lotteries"
 
     def get_queryset(self):
-        now_ts = now()
-        return Lottery.objects.filter(
-            opening_datetime__lte=now_ts,
-            closing_datetime__gte=now_ts,
-        ).order_by("opening_datetime")
+        return Lottery.open_lotteries.all()
