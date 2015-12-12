7eb086c539d237e01799402c0b5256c01d165969
Javier Rojas <jerojasro@devnull.li>
2015-12-12 20:24:51 +0100

    basic model for app

---
+++ lottery/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Lottery(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    opening_datetime = models.DateTimeField(
+        help_text="time after which getting a ticket for this lottery is allowed",
+    )
+    closing_datetime = models.DateTimeField(
+        help_text="time after which getting a ticket for this lottery is no longer allowed",
+    )
+    winner_ticket = models.CharField(
+        max_length=200,
+        help_text="Serialization of the winner ticket/selection of numbers",
+        blank=True,
+    )
+
+
+class LotteryTicket(models.Model):
+    lottery = models.ForeignKey("lottery.Lottery")
+    player = models.ForeignKey("auth.User")
+    ticket = models.CharField(
+        max_length=200,
+        help_text="Serialization of the ticket/selection of numbers made by the player",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
